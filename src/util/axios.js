682aa29d1f6d06c002b24ab1a9eb619e090baf2c
Vedvardhan <vedvardhan.g@gmail.com>
2022-05-16 20:00:28 +0530

    updated backend uri

--- src/util/axios.js
+++ src/util/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const serverInstance = axios.create({
-    baseURL: "https://spotify-magic-playlist-6dy3mwx3y-ved08.vercel.app"
+    baseURL: "https://spotify-magic-playlist-ved.vercel.app/"
 })
 export const spotifyInstance = axios.create({
     baseURL: "https://api.spotify.com/v1"
